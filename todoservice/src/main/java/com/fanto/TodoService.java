73305be1bef714470a601d3d254dfb183b8acfbc
heshuhua <heshuhua@gmail.com>
2016-07-25 10:59:07 +0800

    ip

--- todoservice/src/main/java/com/fanto/TodoService.java
+++ todoservice/src/main/java/com/fanto/TodoService.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/todos")
+@Transactional
 public class TodoService {
 
 	@Autowired
