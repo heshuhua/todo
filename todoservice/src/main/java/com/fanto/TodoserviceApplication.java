1412b9e1e0ce05baa7488c9359166d12731cd600
heshuhua <heshuhua@gmail.com>
2016-07-26 17:59:50 +0800

    zippkin

--- todoservice/src/main/java/com/fanto/TodoserviceApplication.java
+++ todoservice/src/main/java/com/fanto/TodoserviceApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
+@EnableBinding(Sink.class)
 public class TodoserviceApplication {
 	
 	
